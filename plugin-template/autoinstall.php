cbc4f7a1e068f87cb14c283dfe66c37d25fc7d13
Ben <hostellerie.org@gmail.com>
2026-08-05 15:57:09 +0200

    Update autoinstall.php template for Geeklog 2.2.2 structure and pi_gl_version

--- plugin-template/autoinstall.php
+++ plugin-template/autoinstall.php
@@ -1,141 +1,40 @@
 <?php
-
-/* Reminder: always indent with 4 spaces (no tabs). */
-// +---------------------------------------------------------------------------+
-// | Foo Bar Plugin 0.0                                                        |
-// +---------------------------------------------------------------------------+
-// | autoinstall.php                                                           |
-// |                                                                           |
-// | This file provides helper functions for the automatic plugin install.     |
-// +---------------------------------------------------------------------------+
-// | Copyright (C) yyyy by the following authors:                              |
-// |                                                                           |
-// | Authors: author name goes here                                            |
-// +---------------------------------------------------------------------------+
-// | Created with the Geeklog Plugin Toolkit.                                  |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is free software; you can redistribute it and/or             |
-// | modify it under the terms of the GNU General Public License               |
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-// | GNU General Public License for more details.                              |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
-// +---------------------------------------------------------------------------+
-
 /**
-* @package FooBar
-*/
+ * Example autoinstall template for Geeklog 2.2.2+
+ */
 
-/**
-* Plugin autoinstall function
-*
-* @param    string  $pi_name    Plugin name
-* @return   array               Plugin information
-*
-*/
-function plugin_autoinstall_foobar($pi_name)
-{
-    $pi_name         = 'foobar';
-    $pi_display_name = 'Foo Bar';
-    $pi_admin        = $pi_display_name . ' Admin';
-
-    $info = array(
-        'pi_name'         => $pi_name,
-        'pi_display_name' => $pi_display_name,
+$autoinstall['foobar'] = array(
+    'info' => array(
+        'pi_name'         => 'foobar',
+        'pi_display_name' => 'Foo Bar',
         'pi_version'      => '0.0.0',
-        'pi_gl_version'   => '1.6.0',
+        'pi_gl_version'   => '2.2.2', // minimum Geeklog required
         'pi_homepage'     => 'http://www.example.com/'
-    );
-
-    $groups = array(
-        $pi_admin => 'Users in this group can administer the '
-                     . $pi_display_name . ' plugin'
-    );
-
-    $features = array(
-        $pi_name . '.admin'    => 'Full access to ' . $pi_display_name
-                                  . ' plugin'
-    );
-
-    $mappings = array(
-        $pi_name . '.admin'     => array($pi_admin)
-    );
-
-    $tables = array(
+    ),
+    'groups' => array(
+        'Foo Bar Admin' => 'Users in this group can administer the Foo Bar plugin'
+    ),
+    'features' => array(
+        'foobar.admin' => 'Full access to Foo Bar plugin'
+    ),
+    'mappings' => array(
+        'foobar.admin' => array('Foo Bar Admin')
+    ),
+    'tables' => array(
 {optional:use_sql}
         'foobar'
 {/optional:use_sql}
-    );
-
-    // Version control and dependencies settings for Geeklog 1.8.0+
-    // http://wiki.geeklog.net/index.php/Plugin_Autoinstall#Plugin_dependencies_and_version_control
-    $requires = array(
+    ),
+    // plugin dependencies (other plugins) - optional but recommended
+    'requires' => array(
 {optional:use_sql}
-        array('db' => 'mysql', 'version' => '4.1'),
-        array('db' => 'mssql')
+        // Example: require MySQL support (if you depend on a DB)
+        // array('plugin' => 'staticpages', 'version' => '1.7.0')
 {/optional:use_sql}
-    );
-
-    $inst_parms = array(
-        'info'      => $info,
-        'groups'    => $groups,
-        'features'  => $features,
-        'mappings'  => $mappings,
-        'tables'    => $tables,
-        'requires'  => $requires
-    );
-
-    return $inst_parms;
-}
-{optional:use_config_ui}
-/**
-* Create the initial configuration for the plugin
-*/
-function plugin_load_configuration_foobar($pi_name)
-{
-    global $_CONF;
-
-    $base_path = $_CONF['path'] . 'plugins/' . $pi_name . '/';
-
-    require_once $_CONF['path_system'] . 'classes/config.class.php';
-    require_once $base_path . 'install_defaults.php';
-
-    return plugin_initconfig_foobar();
-}
-{/optional:use_config_ui}
-
-/**
-* Check if the plugin is compatible with this Geeklog version
-*
-* @param    string  $pi_name    Plugin name
-* @return   boolean             true: plugin compatible; false: not compatible
-*
-*/
-function plugin_compatible_with_this_version_foobar($pi_name)
-{
-{optional:use_sql}
-    global $_CONF, $_DB_dbms;
-
-    // check if we support the DBMS the site is running on
-    $dbFile = $_CONF['path'] . 'plugins/' . $pi_name . '/sql/'
-            . $_DB_dbms . '_install.php';
-    if (! file_exists($dbFile)) {
-        return false;
-    }
-
-{/optional:use_sql}
-    // add checks here
-
-    return true;
-}
-
+    ),
+    // Alternative map-style dependency declaration:
+    'plugin_dependencies' => array(
+        // 'staticpages' => '1.7.0'
+    )
+);
 ?>
